8c182bde83b9873fc8b15ac7b509ad8a5154cc48
steven-cpp <qwert556@live.cn>
2022-12-07 19:18:22 +0800

    Forward List ops

--- leetcode_src/03_binSearch.cpp
+++ leetcode_src/03_binSearch.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 class Solution {
 public:
+    // nums = [5,7,7,8,8,10], target = 8
+    // output: [3, 4]
     vector<int> searchRange(vector<int>& nums, int target) {
         
     }
